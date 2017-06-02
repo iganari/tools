60f3daf3a6d47a88c9c84f290560e5c7dd3699cd
iganari <cigaguri@gmail.com>
2017-06-02 10:19:01 +0900

    sgを追加

--- VPC/terraform/main.tf
+++ VPC/terraform/main.tf
@@ -5,7 +5,6 @@ variable "region" {
 }
 variable "product_name" {}
 
-# main.tf
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
@@ -69,28 +68,108 @@ resource "aws_route_table_association" "association" {
     subnet_id = "${aws_subnet.subnet-public.id}"
     route_table_id = "${aws_route_table.route-table-public.id}"
 }
- 
+
+resource "aws_security_group" "sg-ssh" {
+    name = "${var.product_name}"
+    description = "Allow traffic on port 22 between ELB to EC2"
+    vpc_id = "${aws_vpc.vpc.id}"
+    tags {
+        Name  = "${var.product_name}-ssh"
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        # security_groups = ["${aws_security_group.sg-ssh.id}"]
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        self = "true"
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
+resource "aws_security_group" "sg-elb-ec2" {
+    name = "${var.product_name}-elb-ec2"
+    description = "Allow traffic on port 80, 443 between ELB to EC2"
+    vpc_id = "${aws_vpc.vpc.id}"
+    tags {
+        Name  = "${var.product_name}-elb-ec2"
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        self = "true"
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        self = "true"
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "sg-ec2-rds" {
+    name = "${var.product_name}-ec2-rds"
+    description = "Allow traffic on port 3306 between EC2 to RDS"
+    # desciription = "Allow all network traffic between A and B"
+    vpc_id = "${aws_vpc.vpc.id}"
+    tags {
+        Name  = "${var.product_name}-ec2-rds"
+    }
+    ingress {
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        self = "true"
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "sg-public-http" {
+    name = "${var.product_name}-public-http"
+    description = "Allow traffic on port 80"
+    vpc_id = "${aws_vpc.vpc.id}"
+    tags {
+        Name  = "${var.product_name}-public-http"
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
 
 
 
-
-# resource "aws_security_group" "admin" {
-#     name = "admin"
-#     description = "Allow SSH inbound traffic"
-#     vpc_id = "${aws_vpc.myVPC.id}"
-#     ingress {
-#         from_port = 22
-#         to_port = 22
-#         protocol = "tcp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-#     egress {
-#         from_port = 0
-#         to_port = 0
-#         protocol = "-1"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-# }
 #  
 # resource "aws_instance" "cm-test" {
 #     ami = "${var.images.ap-northeast-1}"
