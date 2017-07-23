f20fa4b696444e4b9a62bf63bde0e0bd3f0b26fb
iganari <cigaguri@gmail.com>
2017-07-23 19:59:08 +0900

    update jenkins shel

--- jenkins/src/update-jenkins.sh
+++ jenkins/src/update-jenkins.sh
@@ -5,9 +5,23 @@ set -xeu
 _JDIR='/usr/lib/jenkins'
 _VER=`curl http://updates.jenkins-ci.org/download/war/index.html | grep jenkins.war | grep download | head -n1 | awk -F\/ '{print $10}'`
 
-mkdir ${_JDIR}/${_VER}
-wget http://updates.jenkins-ci.org/download/war/${_VER}/jenkins.war -O ${_JDIR}/${_VER}/jenkins.war
 
-unlink ${_JDIR}/jenkins.war
-ln -s ${_JDIR}/${_VER}/jenkins.war ${_JDIR}/jenkins.war
-systemctl stop jenkins && systemctl start jenkins
+update_jenkins()
+{
+  mkdir ${_JDIR}/${_VER}
+  wget http://updates.jenkins-ci.org/download/war/${_VER}/jenkins.war -O ${_JDIR}/${_VER}/jenkins.war
+  
+  unlink ${_JDIR}/jenkins.war
+  ln -s ${_JDIR}/${_VER}/jenkins.war ${_JDIR}/jenkins.war
+  systemctl stop jenkins
+  sleep 10
+  systemctl start jenkins
+}
+
+
+if [ -e ${_JDIR/${_VER}} ]; then
+  echo "directory Exist!"
+else
+  echo "directory  Exist!"
+  update_jenkins
+fi
